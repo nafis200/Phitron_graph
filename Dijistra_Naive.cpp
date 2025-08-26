3b716130144029c15bbc530c5757c88260d95c6c
nafis200 <nafisahamed14@gmail.com>
2025-08-26 21:41:02 +0600

    Naive Dijistra

---
+++ Dijistra_Naive.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<int, int>> g[1005];
+int a[1005];
+
+void Dijstra(int src)
+{
+    queue<pair<int, int>> q;
+    q.push({src, 0});
+    a[src] = 0;
+    while (!q.empty())
+    {
+        auto x = q.front();
+        q.pop();
+        for (auto v : g[x.first])
+        {
+            if (v.second + x.second < a[v.first])
+            {
+                a[v.first] = v.second + x.second;
+                q.push({v.first,a[v.first]});
+            }
+        }
+    }
+}
+
+// 5 8
+// 0 1 10
+// 1 2 1
+// 0 2 7
+// 0 3 4
+// 2 3 1
+// 3 4 5
+// 3 4 5
+// 2 4 5 
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    for (int i = 0; i < 1005; i++)
+    {
+        a[i] = INT_MAX;
+    }
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        g[a].push_back({b, c});
+        g[b].push_back({a, c});
+    }
+    Dijstra(0);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-# abcdgit
+# Dijistra Graph implement Dirrect Graph.
+
+# Single Source Shortest Path.
+
